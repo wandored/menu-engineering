8f45f63ca2eacb2e0ee36dd1df83aa9470dddba6
Robert Kinsella <chefk67@gmail.com>
2020-10-27 18:08:08 -0400

    added pinkkiss

--- pinkkiss.py
+++ pinkkiss.py
@@ -7,7 +7,7 @@ df_product = pd.DataFrame()
 df_cost = pd.DataFrame()
 df_MenuEng = pd.DataFrame()
 
-pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_rows', 600)
 
 
 def removedups(x):
@@ -52,7 +52,6 @@ input('Press "Enter" when you are ready to continue')
 
 # Imports and cleans data into dataframe
 df_product = pd.read_csv('Product Mix.csv', skiprows=3, sep=',', thousands=',')
-print(df_product)
 df_product[['Location', 'MenuItem']
            ] = df_product['TransferDate'].str.split(' - ', expand=True)
 df_product.rename(columns={'Cost': 'Price', 'Total': 'Sales'}, inplace=True)
@@ -79,48 +78,53 @@ df_MenuEng.rename(columns={'Location_x': 'Location'}, inplace=True)
 df_MenuEng.drop(columns={'Location_y', 'AvgPrice'}, inplace=True)
 df_pmix = df_MenuEng.reindex(columns=[
     'Location', 'MenuItem', 'Qty', 'Price', 'Sales', 'Cost', 'Cat1', 'Cat2', 'Cat3'])
-df_MenuEng = df_pmix.drop(df_pmix[df_pmix.Price == 0].index)
+df_MenuEng = df_pmix.drop(df_pmix[df_pmix.MenuItem != 'PINK KISS'].index)
+df_MenuEng.drop_duplicates(subset=['Location'], keep='last', inplace=True)
 
 # Get list of categories from data
-cat1_list = df_MenuEng['Cat1']
-cat1_list = removedups(cat1_list)
-cat2_list = df_MenuEng['Cat2']
-cat2_list = removedups(cat2_list)
-df_MenuEng['FoodCost'] = df_MenuEng.apply(
-    lambda row: row.Cost/row.Price, axis=1)
-df_MenuEng['Margin'] = df_MenuEng.apply(
-    lambda row: row.Price - row.Cost, axis=1)
-df_pmix = df_MenuEng.reindex(columns=['Location', 'MenuItem', 'Qty', 'Price',
-                                      'FoodCost', 'Sales', 'Cost', 'Margin', 'Cat1', 'Cat2', 'Cat3'])
-df_MenuEng = df_pmix
+#cat1_list = df_MenuEng['Cat1']
+#cat1_list = removedups(cat1_list)
+#cat2_list = df_MenuEng['Cat2']
+#cat2_list = removedups(cat2_list)
+# df_MenuEng['FoodCost'] = df_MenuEng.apply(
+#    lambda row: row.Cost/row.Price, axis=1)
+# df_MenuEng['Margin'] = df_MenuEng.apply(
+#    lambda row: row.Price - row.Cost, axis=1)
+# df_pmix = df_MenuEng.reindex(columns=['Location', 'MenuItem', 'Qty', 'Price',
+#                                      'FoodCost', 'Sales', 'Cost', 'Margin', 'Cat1', 'Cat2', 'Cat3'])
+#df_MenuEng = df_pmix
 
-for loc in cat1_list:
-    with pd.ExcelWriter(f'MenuEngineering-{loc}.xlsx') as writer:    # pylint: disable=abstract-class-instantiated
-        for cat in cat2_list:
-            df = menucatagory(cat)
-            df = engineer(df)
-            df['rating'] = df.apply(rating, axis=1)
-            df.sort_values(by='Sales', inplace=True,
-                           ascending=False, ignore_index=True)
-            location = df.loc[0, 'Location']
-            df.drop(columns={'Location', 'Cat3',
-                             'qty_mn', 'mrg_mn'}, inplace=True)
-            print()
-            print(f'{cat} Menu Engineering for {location}')
-            print(df)
-            df.to_excel(writer, sheet_name=cat, index=False)
 
-for loc in cat1_list:
-    with open('MenuEngineering.html', 'w', newline='') as writer:
-        for cat in cat2_list:
-            df = menucatagory(cat)
-            df = engineer(df)
-            df['rating'] = df.apply(rating, axis=1)
-            df.sort_values(by='Sales', inplace=True,
-                           ascending=False, ignore_index=True)
-            location = df.loc[0, 'Location']
-            df.drop(columns={'Location', 'Cat3',
-                             'qty_mn', 'mrg_mn'}, inplace=True)
-            html = df.to_html(justify='center')
-            writer.write(f'Menu Engineering for {cat}s at {location}')
-            writer.write(html)
+#
+# for loc in cat1_list:
+#    with pd.ExcelWriter(f'MenuEngineering-{loc}.xlsx') as writer:    # pylint: disable=abstract-class-instantiated
+#        for cat in cat2_list:
+#            df = menucatagory(cat)
+#            df = engineer(df)
+#            df['rating'] = df.apply(rating, axis=1)
+#            df.sort_values(by='Sales', inplace=True,
+#                           ascending=False, ignore_index=True)
+#            location = df.loc[0, 'Location']
+#            df.drop(columns={'Location', 'Cat3',
+#                             'qty_mn', 'mrg_mn'}, inplace=True)
+#            print()
+#            print(f'{cat} Menu Engineering for {location}')
+#            print(df)
+#            df.to_excel(writer, sheet_name=cat, index=False)
+#
+# for loc in cat1_list:
+#    with open('MenuEngineering.html', 'w', newline='') as writer:
+#        for cat in cat2_list:
+#            df = menucatagory(cat)
+#            df = engineer(df)
+#            df['rating'] = df.apply(rating, axis=1)
+#            df.sort_values(by='Sales', inplace=True,
+#                           ascending=False, ignore_index=True)
+#            location = df.loc[0, 'Location']
+#            df.drop(columns={'Location', 'Cat3',
+#                             'qty_mn', 'mrg_mn'}, inplace=True)
+#            html = df.to_html(justify='center')
+#            writer.write(f'Menu Engineering for {cat}s at {location}')
+#            writer.write(html)
+
+print(df_MenuEng)
